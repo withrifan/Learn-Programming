1e50dbc32fe9c31b3cf0eff6590b9d98537d8389
withrifan <withrifan@gmail.com>
2024-06-26 21:09:36 +0700

    deleted:    python/praktikum5/kabisat.py 	modified:   python/praktikum5/program2-tahun-kabisat.py 	modified:   python/praktikum5/program3-zodiak-yunani.py 	deleted:    python/praktikum5/program3-zodiak-yunani2.py

--- python/praktikum5/program2-tahun-kabisat.py
+++ python/praktikum5/program2-tahun-kabisat.py
@@ -1,6 +1,26 @@
-tahun = int(input("tahun : "))
-
-if(tahun % 2 == 0):
-    print("tahun",tahun,"merupakan tahun kabisat")
+# Menginput Tahun
+tahun = int(input("Tulis Sebuah Tahun: "))
+ 
+#Perulangan Pertama
+if (tahun % 4) == 0:
+ 
+   #Perulangan Kedua
+   if (tahun % 100) == 0:
+ 
+       #Perulangan Ketiga
+       if (tahun % 400) == 0:
+ 
+           #Tergolong Tahun Kabisat
+           print("{0} adalah Tahun Kabisat".format(tahun))
+ 
+       #Bukan Tergolong Tahun Kabisat
+       else:
+           print("{0} bukan Tahun Kabisat".format(tahun))
+ 
+   #Tergolong Tahun Kabisat
+   else:
+       print("{0} adalah Tahun Kabisat".format(tahun))
+ 
+#Bukan Tergolong Tahun Kabisat
 else:
-    print("tahun",tahun,"bukan tahun kabisat")
+   print("{0} bukan Tahun Kabisat".format(tahun))
--- python/praktikum5/program3-zodiak-yunani.py
+++ python/praktikum5/program3-zodiak-yunani.py
@@ -1,18 +1,34 @@
 tgl = int(input("tanggal anda lahir: "))
 bln = str(input("bulan anda lahir: "))
 
-if(bln == "maret"):
-    if(tgl >= 21 and tgl <= 31):
-        print("Astrologi anda adalah Aries")
-    elif(tgl >= 1 and tgl <= 20):
-        print("Astrologi anda adalah Pisces")
-elif(bln == "april"):
-    if(tgl >= 1 and tgl <= 19):
-        print("Astrologi anda adalah Aries")
-    elif(tgl >= 20 and tgl <= 30):
-        print("Astrologi anda adalah Taurus")
-elif(bln == "april"):
-    if(tgl >= 1 and tgl <= 19):
-        print("Astrologi anda adalah Aries")
-    elif(tgl >= 20 and tgl <= 30):
-        print("Astrologi anda adalah Taurus")
+if((bln == "maret" and tgl >= 21 and tgl <= 31) or (bln == "april" and tgl >= 1 and tgl <= 19)):
+    print("Astrologi anda adalah Aries")
+elif((bln == "april" and tgl >= 20 and tgl <= 30) or (bln == "mei" and tgl >= 1 and tgl <= 20)):
+    print("Astrologi anda adalah Taurus")
+elif((bln == "mei" and tgl >= 21 and tgl <= 31) or (bln == "juni" and tgl >= 1 and tgl <= 20)):
+    print("Astrologi anda adalah Gemini")
+elif((bln == "juni" and tgl >= 21 and tgl <= 30) or (bln == "juli" and tgl >= 1 and tgl <= 22)):
+    print("Astrologi anda adalah Cancer")
+elif((bln == "juli" and tgl >= 23 and tgl <= 31) or (bln == "agustus" and tgl >= 1 and tgl <= 22)):
+    print("Astrologi anda adalah Leo")
+elif((bln == "agustus" and tgl >= 23 and tgl <= 31) or (bln == "september" and tgl >= 1 and tgl <= 22)):
+    print("Astrologi anda adalah Virgo")
+elif((bln == "september" and tgl >= 23 and tgl <= 30) or (bln == "oktober" and tgl >= 1 and tgl <= 22)):
+    print("Astrologi anda adalah Libra")
+elif((bln == "oktober" and tgl >= 23 and tgl <= 31) or (bln == "november" and tgl >= 1 and tgl <= 21)):
+    print("Astrologi anda adalah Scorpio")
+elif((bln == "november" and tgl >= 22 and tgl <= 30) or (bln == "desember" and tgl >= 1 and tgl <= 21)):
+    print("Astrologi anda adalah Sagitarius")
+elif((bln == "desember" and tgl >= 22 and tgl <= 31) or (bln == "januari" and tgl >= 1 and tgl <= 19)):
+    print("Astrologi anda adalah Capricorn")
+elif((bln == "januari" and tgl >= 20 and tgl <= 31) or (bln == "februari" and tgl >= 1 and tgl <= 18)):
+    print("Astrologi anda adalah Aquarius")
+elif((bln == "februari" and tgl >= 19 and tgl <= 29) or (bln == "maret" and tgl >= 1 and tgl <= 20)):
+    print("Astrologi anda adalah Pisces")
+else:
+    print("Zodiak anda tidak diketahui atau input yang dimasukkan salah")
+
+
+
+
+    
